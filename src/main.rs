638b86c27cedf0d1f9c4a77707fe65ce72a01d6b
Michiel Missotten <michiel@ouistock.fr>
2018-08-13 22:14:15 +0200

    Adding form integration and splitting in functions

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,34 @@ extern crate askama;
 extern crate openssl;
 extern crate openssl_probe;
 
+extern crate dotenv;
+extern crate log;
+extern crate env_logger;
+
 use actix_web::server;
 use actix_web::App;
 use actix_web::HttpRequest;
+use actix_web::HttpResponse;
+use actix_web::Form;
+use actix_web::http;
+use actix_web::middleware;
+
+use dotenv::dotenv;
+
 use openssl::ssl::SslMethod;
 use openssl::ssl::SslConnector;
+
 use std::net::TcpStream;
 
 use askama::Template; 
+
+#[macro_use]
+extern crate serde_derive;
+
+#[derive(Deserialize)]
+struct DomainForm {
+    domain: String,
+}
 
 struct CertificateInformation {
     domain: String, 
@@ -22,29 +42,15 @@ struct CertificateInformation {
 
 #[derive(Template)] 
 #[template(path = "index.html")]
-struct HelloTemplate<> {
+struct IndexTemplate<> {
     certificate: Option<CertificateInformation>, 
 }
 
 fn get_certificate_info(domain: String) -> Option<CertificateInformation> {
-    Some(CertificateInformation{
-        domain: "ouistock.fr".to_string(), 
-        not_after: "01/01/2017".to_string(), 
-        not_before: "01/02/2017".to_string()
-    })
-}
-
-fn index(_req: &HttpRequest) -> &'static str {
-    "Hello world!"
-}
-
-fn main() {
-    openssl_probe::init_ssl_cert_env_vars();
-
+    let formatted_domain = format!("{}:443", domain);
     let connector = SslConnector::builder(SslMethod::tls()).unwrap().build(); 
-    let stream = TcpStream::connect("www.ouistock.fr:443").unwrap();
-    let connection = connector.connect("www.ouistock.fr", stream).unwrap();
-      
+    let stream = TcpStream::connect(formatted_domain).unwrap();
+    let connection = connector.connect(&domain, stream).unwrap();
     
     let ssl = connection.ssl();
     let certificate = ssl.peer_certificate().unwrap();
@@ -52,7 +58,39 @@ fn main() {
     println!("{}", certificate.not_before());
     println!("{}", certificate.not_after());
 
-    server::new(|| App::new().resource("/", |r| r.f(index)))
+    Some(CertificateInformation{
+        domain: domain, 
+        not_after: certificate.not_after().to_string(), 
+        not_before: certificate.not_before().to_string()
+    })
+}
+
+fn index(_req: &HttpRequest) -> HttpResponse {
+    let template = IndexTemplate { certificate: None,}.render().unwrap();
+
+    HttpResponse::Ok().body(template)
+}
+
+fn handle_post((_req, params): (HttpRequest, Form<DomainForm>),) -> HttpResponse {
+    let certificate = get_certificate_info(params.domain.clone());
+    let template = IndexTemplate { certificate: certificate }.render().unwrap();
+
+    HttpResponse::Ok().body(template)
+}
+
+fn main() {
+    openssl_probe::init_ssl_cert_env_vars();
+    dotenv().ok();
+    env_logger::init();
+ 
+    server::new(|| 
+        App::new()
+            .middleware(middleware::Logger::default())
+            .resource("/", |r| {
+                r.method(http::Method::GET).f(index);
+                r.method(http::Method::POST).with(handle_post);
+            })
+        )
         .bind("127.0.0.1:8088")
         .unwrap()
         .run();
